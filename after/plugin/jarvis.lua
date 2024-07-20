086f5e2d452b99cad791353a650f4a67f22be705
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-07-20 11:06:06 -0400

    Update jarvis config

--- after/plugin/jarvis.lua
+++ after/plugin/jarvis.lua
@@ -1,7 +1,7 @@
 local model_name = "gpt-4o"
 local url = "https://api.openai.com/v1/chat/completions"
 local api_key_name = "OPENAI_API_KEY"
-local system_prompt = "You are my helpful assistant coder. Try to be as non-verbose as possible and stick to the important things. Avoid describing your own code unnecessarily, I only want you to output code mainly and limit describing it."
+local system_prompt = "You are my helpful assistant coder. Try to be as non-verbose as possible and stick to the important things. Avoid describing your own code unnecessarily, I only want you to output code mainly and limit describing it. Avoid reprinting my code at all costs, I want to see changes or re-written functions, but never just re-print my code or code from past interactions unless absolutely necessary."
 
 local function openai_data_handler(data_stream)
     if data_stream:match '"delta":' then
@@ -13,9 +13,21 @@ local function openai_data_handler(data_stream)
 end
 
 local function make_openai_curl_args(history, prompt)
+    -- ARGS 
+    -- history: a table of {{role=..., content=...}} 
+    --          role: can be either "assistant" or "user"
+    --          content: is a string of the relevant content
+    -- prompt: is the string of the current prompt
     local api_key = os.getenv(api_key_name)
+    local messages = {
+        { role = 'system', content = system_prompt },
+    }
+    for _, row in ipairs(history) do
+        table.insert(messages, { role=row.role, content=row.content })
+    end
+    table.insert(messages, { role='user', content=prompt })
     local data = {
-        messages = { { role = 'system', content = system_prompt }, { role = 'user', content = history .. prompt } },
+        messages = messages,
         model = model_name,
         temperature = 0.7,
         stream = true,
@@ -30,9 +42,18 @@ local function make_openai_curl_args(history, prompt)
 end
 
 require("jarvis").setup({
-    cache_limit=1000,
+    -- defaults
     data_handler=openai_data_handler,
     make_curl_args=make_openai_curl_args,
+    -- prune_after = 30,
+    -- cache_limit = 1000,
+    -- keymaps = {
+    --     close = "<esc>", -- close the ui
+    --     new_chat = "<C-n>", -- create new persistent, file-agnostic chat file
+    --     switch_window = "<C-s>", -- switch between prompt and history window
+    --     run = "<C-e>", -- run model
+    --     copy_and_close = "<C-y>" -- copy visual selection in history window and close
+    -- },
 })
 
 vim.keymap.set({ 'n', 'v' }, '<leader>lc', function() require("jarvis").interact("chat") end, { desc = 'ask jarvis' })
