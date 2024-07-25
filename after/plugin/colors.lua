22b514c51b2055ee5a98fed0c6fa3028e46f9579
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-07-24 23:21:49 -0400

    Update colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,14 @@
 --
 function ColorMyPencils()
-    -- vim.cmd.colorscheme("alabaster")
-    -- vim.opt.background = "dark"
-
-    vim.g.sonokay_style = "andromeda"
-    vim.g.sonokai_better_performance=1
-    vim.cmd.colorscheme("sonokai")
+    vim.cmd.colorscheme("alabaster")
     vim.opt.background = "dark"
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    -- vim.g.sonokay_style = "andromeda"
+    -- vim.g.sonokai_better_performance=1
+    -- vim.cmd.colorscheme("sonokai")
+    -- vim.opt.background = "dark"
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
