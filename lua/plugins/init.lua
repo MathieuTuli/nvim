14286f53f814f6edbcccc270ab93deec5029ea1d
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-09-05 14:34:26 -0400

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -164,18 +164,4 @@ return {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
-    {
-        "goolord/alpha-nvim",
-        -- dependencies = { 'echasnovski/mini.icons' },
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-            local startify = require("alpha.themes.startify")
-            -- available: devicons, mini, default is mini
-            -- if provider not loaded and enabled is true, it will try to use another provider
-            startify.file_icons.provider = "devicons"
-            require("alpha").setup(
-            startify.config
-            )
-        end,
-    },
 }
