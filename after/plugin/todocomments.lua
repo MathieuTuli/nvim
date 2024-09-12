698dfead869a15497ebdc597d3bd30006c3db8dc
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-09-12 11:38:37 -0400

    Update todo elements

--- after/plugin/todocomments.lua
+++ after/plugin/todocomments.lua
@@ -7,11 +7,12 @@ require('todo-comments').setup(
             alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
             -- signs = false,
         },
-        TODO = { icon = " ", color = "#88cc66" },
+        TODO = { icon = "❇ ", color = "#88cc66" },
         HACK = { icon = " ", color = "#cc8bc9" },
         WARN = { icon = " ", color = "#f4b371", alt = { "WARNING", "XXX" } },
         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
         NOTE = { icon = " ", color = "#8ebeec", alt = { "INFO" } },
+        DONE = { icon = " ", color = "#6d6d6d" },
         TEST = { icon = "- ", color = "#9d9d9d", alt = { "TESTING", "PASSED", "FAILED" } },
     },
     colors = {
